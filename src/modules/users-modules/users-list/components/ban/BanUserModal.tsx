8b57259da1284959d5140bd0fe8922bde96529ab
Anna <anna.blackbird1@gmail.com>
2023-06-28 00:02:43 +0700

    feat: add ban and delete modals with localization

---
+++ src/modules/users-modules/users-list/components/ban/BanUserModal.tsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react'
+
+import { useTranslation } from 'react-i18next'
+
+import { ModalWithContent } from '@/components/modals'
+import { ImageOption } from '@/ui/image-selector/image-option/ImageOption'
+import { ImageSelector } from '@/ui/image-selector/ImageSelector'
+
+type PropsType = {
+  isBanUserOpen: boolean
+  setIsBanUserOpen: (isBanUserOpen: boolean) => void
+  defaultText: string
+  userId: number
+  userName: string
+}
+export const BanUserModal = ({
+  isBanUserOpen,
+  setIsBanUserOpen,
+  defaultText,
+  userId,
+  userName,
+}: PropsType) => {
+  const { t } = useTranslation()
+
+  const reasons = [
+    { text: t('userList.ban.reason.anotherReason') },
+    { text: t('userList.ban.reason.behavior') },
+    { text: t('userList.ban.reason.advertising') },
+  ]
+
+  const [isOpen, setIsOpen] = useState(false)
+  const [banReason, setBanReason] = useState(defaultText)
+
+  const onDropdownClick = () => {
+    setIsOpen(!isOpen)
+  }
+
+  const onDecline = () => {
+    setIsBanUserOpen(false)
+    setIsOpen(false)
+  }
+
+  const onConfirm = () => {
+    console.log('ban onConfirm')
+    setIsBanUserOpen(false)
+    setIsOpen(false)
+  }
+
+  const onOptionClick = (text: string) => {
+    setBanReason(text)
+    setIsOpen(false)
+  }
+
+  return (
+    <ModalWithContent
+      isOpen={isBanUserOpen}
+      onClose={onDecline}
+      title={t('userList.ban.title')}
+      confirmButtonText={t('userList.ban.confirm')}
+      declineButtonText={t('userList.ban.cancel')}
+      onConfirm={onConfirm}
+      onDecline={onDecline}
+    >
+      <div>
+        <h3>{t('userList.ban.description') + ' ' + userName + '?'}</h3>
+        <span>{`${t('userList.ban.reason.title')}:`}</span>
+        <div className={'flex justify-end'}>
+          <ImageSelector isOpen={isOpen} setIsOpen={onDropdownClick} chosenText={banReason}>
+            {reasons.map(({ text }) => {
+              return <ImageOption text={text} onOptionClick={() => onOptionClick(text)} />
+            })}
+          </ImageSelector>
+        </div>
+      </div>
+    </ModalWithContent>
+  )
+}
---
+++ src/modules/users-modules/users-list/components/delete-modal/DeleteModal.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+import { useTranslation } from 'react-i18next'
+
+import { Confirm } from '@/components/modals'
+
+type PropsType = {
+  isDeleteUserOpen: boolean
+  setIsDeleteUserOpen: (isDeleteUserOpen: boolean) => void
+  userName: string
+  userId: number
+}
+export const DeleteModal = ({
+  isDeleteUserOpen,
+  setIsDeleteUserOpen,
+  userName,
+  userId,
+}: PropsType) => {
+  const { t } = useTranslation()
+
+  const onConfirm = () => {
+    console.log('delete onConfirm')
+    setIsDeleteUserOpen(false)
+  }
+
+  const onDecline = () => {
+    setIsDeleteUserOpen(false)
+  }
+
+  return (
+    <Confirm
+      isOpen={isDeleteUserOpen}
+      onConfirm={onConfirm}
+      onClose={onDecline}
+      onDecline={onDecline}
+      confirmButtonText={t('userList.delete.confirm')}
+      declineButtonText={t('userList.delete.cancel')}
+      title={t('userList.delete.title')}
+      text={t('userList.delete.description') + ' ' + userName + '?'}
+    />
+  )
+}
