d27cd44bf19c1cfa2d54658de7156506c388d2fc
SoundShocking <nireemk@gmail.com>
2023-07-09 20:59:58 +0300

    feat (users): table skeleton

--- src/apollo-client.ts
+++ src/apollo-client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 export const apolloClient = new ApolloClient({
-  uri: 'https://chips.jubo.com.ua/graphql',
+  uri: 'https://api.cygan.lol/graphql',
   cache: new InMemoryCache(),
   headers: {
     authorization: 'Basic YWRtaW5AYWRtaW4ubWU6YWRtaW4=',
