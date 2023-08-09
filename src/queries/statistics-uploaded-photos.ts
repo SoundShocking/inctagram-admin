a5c92e1e267dda87321d0afb7173c400daf55bfc
Anna <anna.blackbird1@gmail.com>
2023-08-09 19:04:44 +0300

    feat: update

--- src/queries/statistics-uploaded-photos.ts
+++ src/queries/statistics-uploaded-photos.ts
@@ -1,14 +1,5 @@
 import { gql } from '@apollo/client'
 
-type StatisticsMetricsImages = {
-  countImages: number[]
-  total_rows: number
-  time_intervals: Date[]
-  maxCountImages: number
-  maxRoundImages: number
-  sumImages: number
-}
-
 export const GET_UPLOADED_PHOTOS_STATISTICS = gql`
   query uploadedImagesStatistics(
     $startDate: Timestamp!
@@ -46,24 +37,6 @@ export const GET_UPLOADED_PHOTOS_STATISTICS = gql`
           sumImages
         }
       }
-      #      data {
-      #        metrics: {
-      #      countImages
-      #      total_rows
-      #      time_intervals
-      #      maxCountImages
-      #      maxRoundImages
-      #      sumImages
-      #  }
-      #        metricsComparison: {
-      #  countImages
-      #  total_rows
-      #  time_intervals
-      #  maxCountImages
-      #  maxRoundImages
-      #  sumImages
-      #  }
-      #      }
     }
   }
 `
