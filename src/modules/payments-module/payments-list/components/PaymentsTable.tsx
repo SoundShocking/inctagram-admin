40335e9651fcdf8d83a4bc688d5735c35e5c7735
SoundShocking <nireemk@gmail.com>
2023-08-10 16:24:00 +0300

    refactor layout

--- src/modules/payments-module/payments-list/components/PaymentsTable.tsx
+++ src/modules/payments-module/payments-list/components/PaymentsTable.tsx
@@ -26,6 +26,7 @@ export type PaymentsItem = Pick<
   | 'amount'
   | 'typeSubscription'
   | 'paymentTypeText'
+  | 'status'
 >
 
 const columnHelper = createColumnHelper<PaymentsItem>()
@@ -98,23 +99,21 @@ export const PaymentsTable: FC<Props> = ({ payments, sorting, setSorting, loadin
         ></div>
       )}
       <table className="w-full grid text-white text-sm">
-        <thead
-          className={
-            'h-12 bg-dark-500 border-2 border-dark-500 border-r-2 font-semibold text-sm grid'
-          }
-        >
+        <thead className="h-12 border-0 bg-dark-500 font-semibold grid">
           {table.getHeaderGroups().map((headerGroup, key) => (
             <tr key={key} className="grid grid-cols-[1fr_180px_180px_180px_180px] items-center">
               {headerGroup.headers.map(header => (
-                <th key={header.id} colSpan={header.colSpan}>
+                <th key={header.id} colSpan={header.colSpan} className="py-0 px-1.5">
                   {header.isPlaceholder ? null : (
                     <div
-                      className={clsx('flex items-center justify-center select-none h-full', {
+                      className={clsx('flex items-center select-none', {
                         'cursor-pointer': header.column.getCanSort(),
                       })}
                       onClick={header.column.getToggleSortingHandler()}
                     >
-                      {flexRender(header.column.columnDef.header, header.getContext())}
+                      <div className="truncate">
+                        {flexRender(header.column.columnDef.header, header.getContext())}
+                      </div>
 
                       <TableSortIcon
                         isCanSort={header.column.getCanSort()}
@@ -132,14 +131,12 @@ export const PaymentsTable: FC<Props> = ({ payments, sorting, setSorting, loadin
           {table.getRowModel().rows.map(row => {
             return (
               <tr
-                className={
-                  'border-[1px] border-dark-500 grid grid-cols-[1fr_180px_180px_180px_180px] items-center'
-                }
+                className="border-0 grid grid-cols-[1fr_180px_180px_180px_180px] items-center"
                 key={row.id}
               >
                 {row.getVisibleCells().map(cell => {
                   return (
-                    <td className={'py-3 px-1.5 text-center overflow-hidden'} key={cell.id}>
+                    <td className="py-3 px-1.5 overflow-hidden" key={cell.id}>
                       <div>{flexRender(cell.column.columnDef.cell, cell.getContext())}</div>
                     </td>
                   )
